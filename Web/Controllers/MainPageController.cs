553dc50b87c492dd3bdfa35e663de89f91b6dbb8
Evgeniy Ogienko <mister.ogienko@gmail.com>
2017-12-15 03:08:03 +0300

    Поправил блок поиска на главной странице

--- Web/Controllers/MainPageController.cs
+++ Web/Controllers/MainPageController.cs
@@ -13,60 +13,60 @@ namespace Web.Controllers {
 
         public ActionResult Index() {
 
-            BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
-                Type = OfferTypeEnum.Donor,
-                Offer = new OfferModel {
-                    Description = "Тест №1",
-                    Category = CategoryEnum.FirstNegative,
-                    City = 31,
-                    Contact = "+7 (910) 745 06 76",
-                    Name = "Огиенко Е.Е."
-                }
-            });
+            //BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
+            //    Type = OfferTypeEnum.Donor,
+            //    Offer = new OfferModel {
+            //        Description = "Тест №1",
+            //        Category = CategoryEnum.FirstNegative,
+            //        City = 31,
+            //        Contact = "+7 (910) 745 06 76",
+            //        Name = "Огиенко Е.Е."
+            //    }
+            //});
 
-            BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
-                Type = OfferTypeEnum.Donor,
-                Offer = new OfferModel {
-                    Description = "Тест №2",
-                    Category = CategoryEnum.FirstPositive,
-                    City = 31,
-                    Contact = "+7 (910) 745 06 76",
-                    Name = "Огиенко Е.Е."
-                }
-            });
+            //BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
+            //    Type = OfferTypeEnum.Donor,
+            //    Offer = new OfferModel {
+            //        Description = "Тест №2",
+            //        Category = CategoryEnum.FirstPositive,
+            //        City = 31,
+            //        Contact = "+7 (910) 745 06 76",
+            //        Name = "Огиенко Е.Е."
+            //    }
+            //});
 
-            BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
-                Type = OfferTypeEnum.Donor,
-                Offer = new OfferModel {
-                    Description = "Тест №3",
-                    Category = CategoryEnum.FourthNegative,
-                    City = 2,
-                    Contact = "+7 (910) 745 06 76",
-                    Name = "Огиенко Е.Е."
-                }
-            });
+            //BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
+            //    Type = OfferTypeEnum.Donor,
+            //    Offer = new OfferModel {
+            //        Description = "Тест №3",
+            //        Category = CategoryEnum.FourthNegative,
+            //        City = 2,
+            //        Contact = "+7 (910) 745 06 76",
+            //        Name = "Огиенко Е.Е."
+            //    }
+            //});
 
-            BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
-                Type = OfferTypeEnum.Recipient,
-                Offer = new OfferModel {
-                    Description = "Тест №4",
-                    Category = CategoryEnum.FirstNegative,
-                    City = 1,
-                    Contact = "+7 (910) 745 06 76",
-                    Name = "Огиенко Е.Е."
-                }
-            });
+            //BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
+            //    Type = OfferTypeEnum.Recipient,
+            //    Offer = new OfferModel {
+            //        Description = "Тест №4",
+            //        Category = CategoryEnum.FirstNegative,
+            //        City = 1,
+            //        Contact = "+7 (910) 745 06 76",
+            //        Name = "Огиенко Е.Е."
+            //    }
+            //});
 
-            BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
-                Type = OfferTypeEnum.Recipient,
-                Offer = new OfferModel {
-                    Description = "Тест №5",
-                    Category = CategoryEnum.FirstNegative,
-                    City = 31,
-                    Contact = "+7 (910) 745 06 76",
-                    Name = "Огиенко Е.Е."
-                }
-            });
+            //BloodSearchModelsRemoteProvider.AddOfferSync(new AddOfferModel {
+            //    Type = OfferTypeEnum.Recipient,
+            //    Offer = new OfferModel {
+            //        Description = "Тест №5",
+            //        Category = CategoryEnum.FirstNegative,
+            //        City = 31,
+            //        Contact = "+7 (910) 745 06 76",
+            //        Name = "Огиенко Е.Е."
+            //    }
+            //});
 
             return View();
         }
