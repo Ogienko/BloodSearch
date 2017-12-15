f0098db2651bd0b8f0e265e7d5e164cdd009f37c
Evgeniy Ogienko <mister.ogienko@gmail.com>
2017-12-15 04:20:41 +0300

    Сверстал карточку

--- Web/Controllers/ItemController.cs
+++ Web/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using BloodSearch.Models.Api;
+using BloodSearch.Models.Api.Models.Offers.Requests;
 using System.Web.Mvc;
 
 namespace Web.Controllers {
@@ -6,7 +7,13 @@ namespace Web.Controllers {
     public class ItemController : Controller {
 
         public ActionResult Index(long id) {
-            var model = BloodSearchModelsRemoteProvider.GetOffer(id);
+            var model = new GetOfferResult();
+            try {
+                 model = BloodSearchModelsRemoteProvider.GetOffer(id);
+            }
+            catch {
+                return View("Error");
+            }
             return View(model);
         }
     }
